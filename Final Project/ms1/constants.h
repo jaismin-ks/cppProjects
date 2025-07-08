f38f4433a859672b3caba1a5c755e159e6ee4714
Jaismin <jaisminks06@gmail.com>
2025-07-08 11:25:24 -0400

    Add files via upload

---
+++ Final Project/ms1/constants.h
@@ -0,0 +1,26 @@
+/* Citation and Sources...
+Final Project Milestone 1: 
+Module: constants
+Filename: constants.h
+Version 1.0
+Author: Jaismin Kaur, StNo: 129603247, Email: jksidhu35@myseneca.ca
+
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2025/7/7  Preliminary release
+2025/?/?  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#ifndef SENECA_CONSTANTS_H
+#define SENECA_CONSTANTS_H
+
+#include <cstddef> 
+
+namespace seneca {
+   const size_t MaximumNumberOfMenuItems = 20u;
+}
+
+#endif // !SENECA_CONSTANTS_H
